b2c7147fa3191f88edeb8e5dd736d3db19efa3fb
johnnywiller <johnnywiller10@gmail.com>
2015-10-16 10:19:55 -0300

    change in readwrite conditions to manage url queue properly

--- src/br/com/effacious/url/URLQueue.java
+++ src/br/com/effacious/url/URLQueue.java
@@ -3,7 +3,10 @@ package br.com.effacious.url;
 import java.net.URL;
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,11 +20,15 @@ import java.util.logging.Logger;
 public class URLQueue {
 
 	private static final int MAX_URLS = 100;
-	
+
 	private static URLQueue instance = new URLQueue();
-	
+
 	private Queue<URL> queue;
-	private ReadWriteLock lock;
+
+	private Lock writeLock;
+	private Lock readLock;
+
+	private Object isEmpty;
 
 	public int size() {
 		return queue.size();
@@ -29,57 +36,75 @@ public class URLQueue {
 
 	private URLQueue() {
 		this.queue = new ArrayDeque<URL>();
-		this.lock = new ReentrantReadWriteLock();
+		this.readLock = new ReentrantLock();
+		this.writeLock = new ReentrantLock();
+		this.isEmpty = new Object();
 	}
 
 	public static URLQueue getInstance() {
 		return instance;
 	}
 
-	public synchronized boolean isEmpty() {
+	public boolean isEmpty() {
 		return queue.isEmpty();
 	}
 
-	public synchronized boolean isFull() {
+	public boolean isFull() {
 		return queue.size() > MAX_URLS;
 	}
 
-	public synchronized void add(URL url) {
+	public void add(URL url) {
 
-		// TODO here we will to make some machanism to store the urls on the
-		// database because if wait() is called could have an deadlock in some
-		// specific cases
-		if (isFull()) {
-			return;
+		writeLock.lock();
+		try {
+
+			// TODO here we will to make some machanism to store the urls on the
+			// database because if wait() is called could have an deadlock in
+			// some
+			// specific cases
+			if (isFull()) {
+				return;
+			}
+
+			// TODO we will use isFull in the future, after fix TODO described
+			// above
+			/*
+			 * while (isFull()) { wait(); }
+			 */
+
+			if (queue.add(url)) {
+				System.err.println("URL enqueued: " + url);
+				Logger.getGlobal().log(Level.INFO, "URL enqueued: " + url);
+			}
+
+			synchronized (isEmpty) {
+				isEmpty.notifyAll();
+			}
+
+		} finally {
+			writeLock.unlock();
 		}
 
-		// TODO we will use isFull in the future, after fix TODO described above
-		/*
-		 * while (isFull()) { wait(); }
-		 */
-		
-		if (queue.add(url)) {
-			System.err.println("URL enqueued: " + url);
-			Logger.getGlobal().log(Level.INFO, "URL enqueued: " + url);
-		}
-		notifyAll();
 	}
 
-	public synchronized URL pop() throws InterruptedException {
+	public URL pop() throws InterruptedException {
+
+		readLock.lock();
+
 		while (isEmpty()) {
-			wait();
+
+			synchronized (isEmpty) {
+				isEmpty.wait();
+			}
+
 		}
-		
-		lock.readLock().lock();
-		
+
 		try {
 			URL url = queue.poll();
-			
-			notifyAll();
-			
+
 			return url;
 		} finally {
-			lock.readLock().unlock();
+			readLock.unlock();
 		}
 	}
 }
