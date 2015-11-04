ce0b4dda93adeb10d5ec47cea4b5b1bb7be53ac2
jejung <jean.jung@rocketmail.com>
2015-11-04 08:16:09 -0200

    Adding previous uncommitted files.
    Signed-off-by: jejung <jean.jung@rocketmail.com>

--- src/br/com/efficacious/media/MediaList.java
+++ src/br/com/efficacious/media/MediaList.java
@@ -7,21 +7,29 @@ import java.net.URLConnection;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
+import br.com.efficacious.crawler.WebCrawler;
+
 /**
+ * A media {@link URLConnection} list to save the medias that will be stored on the index 
+ * given by {@link WebCrawler} config.
+ * 
  * @author Jean Jung
- *
  */
 public class MediaList {
 	
 	private Queue<URLConnection> medias;
 	
 	/**
-	 * 
+	 * Default constructor
 	 */
 	public MediaList() {
 		this.medias = new ArrayBlockingQueue<>(100);
 	}
 	
+	/**
+	 * Add a {@link URLConnection} to the list.
+	 * @param media
+	 */
 	public synchronized void add(URLConnection media) {
 		this.medias.add(media);
 	}
